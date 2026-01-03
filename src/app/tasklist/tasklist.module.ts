833f29b1aae1f79aa5f16c3ad7c146d49462eb41
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-02 21:52:18 -0600

    feature 2: creacion de modulo, componente y declaración de lista de tareas

---
+++ src/app/tasklist/tasklist.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TasklistComponent } from './tasklist/tasklist.component';
+
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [],
+  declarations: [TasklistComponent],
+  providers: [],
+})
+export class TaskListModule { }
---
+++ src/app/tasklist/tasklist/tasklist.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasklist',
+  templateUrl: './tasklist.component.html',
+  styleUrl: './tasklist.component.css'
+})
+export class TasklistComponent {
+
+  public tareas: string[] = [
+    "Tender la cama al despertar",
+    "Bañarse y asearse",
+    "Preparar o desayunar algo saludable",
+    "Revisar mensajes y correos importantes",
+    "Limpiar o acomodar el área de trabajo/estudio",
+    "Realizar alguna actividad física (caminar, gym, estiramientos)",
+    "Avanzar en una tarea de estudio o trabajo",
+    "Comer de forma balanceada",
+    "Lavar platos o recoger la cocina",
+    "Preparar lo necesario para el día siguiente (ropa, mochila, pendientes)",
+  ]
+
+}
